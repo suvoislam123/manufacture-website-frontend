31f426751bd72256458a308eb5a560aae43726d3
suvoislam123 <suvoislam753@gmail.com>
2022-05-28 01:13:14 +0600

    react hook form added

--- src/components/standAlonComponents/Home/Home.js
+++ src/components/standAlonComponents/Home/Home.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import useProducts from '../../../hooks/useProducts';
 import './Home.css'
 import Product from './Product/Product';
 const Home = () => {
-    const [products, setProducts] = useProducts()
+    const [products, setProducts] = useProducts();
+//     const { isLoading, error, data:products } = useQuery('repoData', () =>
+//      fetch('http://localhost:5000/products').then(res =>
+//        res.json()
+//      )
+//    )
+ 
     return (
         <div>
             <div className='banner-container'>
--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
+import { useQueryClient } from "react-query";
 
 const useProducts = () => {
+    const queryclient = useQueryClient();
     const [products, setProducts] = useState([]);
     useEffect(() => {
         fetch('http://localhost:5000/products')
